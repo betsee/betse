Animations working. Fixed bug in cell area calculation.

betse/maindriver.py
@@ -3,7 +3,6 @@
 # Copyright 2014-2015 by Alexis Pietak & Cecil Curry
 # See "LICENSE" for further details.
 
-# FIXME do something about case where data doesn't vary so colorbar isn't useless
 
 import matplotlib.pyplot as plt
 import time
@@ -15,6 +14,7 @@ from betse.science import visualize as viz
 import matplotlib.cm as cm
 from betse.science import filehandling as fh
 import numpy as np
+from matplotlib import animation
 
 class MainDriver(object):
 
@@ -22,8 +22,8 @@ class MainDriver(object):
 
         self.start_time = time.time()  # get a start value for timing the simulation
 
-        self.cells = World(vorclose='circle',worldtype='full')  # always need instance of world
-        self.cells.makeWorld()     # call functions to create the world
+        #self.cells = World(vorclose='circle',worldtype='full')  # always need instance of world
+        #self.cells.makeWorld()     # call functions to create the world
 
         #fh.saveSim(self.cells.savedWorld,self.cells)   # save the world to cache
 
@@ -33,11 +33,11 @@ class MainDriver(object):
 
         self.sim = Simulator(self.p)   # whether running from scratch or loading, instance needs to be called
 
-        self.sim.baseInit(self.cells, self.p)   # initialize data if working from scratch
+        #self.sim.baseInit(self.cells, self.p)   # initialize data if working from scratch
 
-        self.sim.runInit(self.cells,self.p)     # run and save an initialization if working from scratch
+        #self.sim.runInit(self.cells,self.p)     # run and save an initialization if working from scratch
 
-        #self.sim,self.cells, self.p = fh.loadSim(self.sim.savedInit)  # load an initialization from cache
+        self.sim,self.cells, _ = fh.loadSim(self.sim.savedInit)  # load an initialization from cache
 
         self.sim.runSim(self.cells,self.p,save=False)   # run and save the simulation
 
@@ -45,35 +45,37 @@ class MainDriver(object):
 
         vdata0 =self.sim.vm_to*1000
         vdata = self.sim.vm_time[-1]*1000
+        # print('vdata0',vdata0)
+        # print('vdata',vdata)
+        self.meanV0 = round(np.mean(vdata0,axis=0),2)
+        self.meanV = round(np.mean(vdata,axis=0),2)
+
+        vdata_t = np.multiply(self.sim.vm_time,1000)
+
+        viz.AnimateCellData(self.cells,vdata_t,self.sim.time,self.p, save=False, ani_repeat=True)
+        # a.cb.set_label('Voltage [mV]')
+
+
+
+
+
+        # figI, axI, axcbI = viz.plotPolyData(self.cells,clrmap = cm.coolwarm,zdata=vdata0)
+        # figI, axI, _ = viz.plotConnectionData(self.cells, fig=figI, ax = axI, zdata=self.sim.gjopen, pickable=False)
+        # axI.set_ylabel('Spatial y [um]')
+        # axI.set_xlabel('Spatial x [um]')
+        # tit = 'Cell voltage at time zero'+' '+'(average ' + str(self.meanV0) + ' ' + 'mV' + ')'
+        # axI.set_title(tit)
+        # axcbI.set_label('Voltage [mV]')
+        # plt.show(block=True)
         #
-        # print(vdata0)
-        # print(vdata)
-
-        figI, axI, axcbI = viz.plotPolyData(self.cells,clrmap = cm.coolwarm,zdata=vdata0)
-        figI, axI, _ = viz.plotConnectionData(self.cells, fig=figI, ax = axI, zdata=self.sim.gjopen, pickable=False)
-        axI.set_ylabel('Spatial y [um]')
-        axI.set_xlabel('Spatial x [um]')
-        axI.set_title('Cell voltage at time zero')
-        if axcbI != None:
-            axcbI.set_label('Voltage [mV]')
-        # for i, pt in enumerate(self.cells.cell_centres):
-        #     axI.text(self.p.um*pt[0],self.p.um*pt[1],i)
-        # if axcbI == None:
-        #     md = np.mean(vdata0,axis=0)
-        #     axI.text(0,0,md)
-        plt.show(block=False)
-
-        figV, axV, axcbV = viz.plotPolyData(self.cells,clrmap = cm.coolwarm,zdata=vdata)
-        figV, axV, _ = viz.plotConnectionData(self.cells, fig=figV, ax = axV, zdata=self.sim.gjopen, pickable=False)
-        axV.set_ylabel('Spatial y [um]')
-        axV.set_xlabel('Spatial x [um]')
-        axV.set_title('Voltage in Each Discrete Cell')
-        if axcbV != None:
-            axcbV.set_label('Voltage [mV]')
-        # if axcbV == None:
-        #     md = np.mean(vdata,axis=0)
-        #     axV.text(0,0,md)
-        plt.show(block=False)
+        # figV, axV, axcbV = viz.plotPolyData(self.cells,clrmap = cm.coolwarm,zdata=vdata)
+        # figV, axV, _ = viz.plotConnectionData(self.cells, fig=figV, ax = axV, zdata=self.sim.gjopen, pickable=False)
+        # axV.set_ylabel('Spatial y [um]')
+        # axV.set_xlabel('Spatial x [um]')
+        # tit = 'Cell voltage at time zero'+' '+'(average ' + str(self.meanV) + ' ' + 'mV' + ')'
+        # axV.set_title(tit)
+        # axcbV.set_label('Voltage [mV]')
+        # plt.show(block=True)
 
         # interact.PolyPicker(self.cells,self.p,self.resume_after_picking)
 
@@ -92,8 +94,8 @@ class MainDriver(object):
         # cdata = sim.cc_time[-1][ioni]
         # ionname = sim.ionlabel[ioni]
         #
-        # figNa, axNa, axcbNa = viz.plotPolyData(cells, clrmap = cm.coolwarm,zdata=cdata)
-        # figNa, axNa, _ = viz.plotConnectionData(cells, fig=figNa, ax = axNa, zdata=sim.gjopen)
+        # figNa, axNa, axcbNa = viz.plotPolyData(cells, clrmap = cm.coolwarm,zdata_t=cdata)
+        # figNa, axNa, _ = viz.plotConnectionData(cells, fig=figNa, ax = axNa, zdata_t=sim.gjopen)
         # axNa.set_ylabel('Spatial y [um]')
         # axNa.set_xlabel('Spatial x [um]')
         # tit = ionname + ' ' + 'Concentration in Cells'
@@ -106,8 +108,8 @@ class MainDriver(object):
         # cdata = sim.cc_time[-1][ioni]
         # ionname = sim.ionlabel[ioni]
         #
-        # figK, axK, axcbK = viz.plotPolyData(cells, clrmap = cm.coolwarm,zdata=cdata)
-        # figK, axK, _ = viz.plotConnectionData(cells, fig=figK, ax = axK, zdata=sim.gjopen)
+        # figK, axK, axcbK = viz.plotPolyData(cells, clrmap = cm.coolwarm,zdata_t=cdata)
+        # figK, axK, _ = viz.plotConnectionData(cells, fig=figK, ax = axK, zdata_t=sim.gjopen)
         # axK.set_ylabel('Spatial y [um]')
         # axK.set_xlabel('Spatial x [um]')
         # tit = ionname + ' ' + 'Concentration in Cells'
@@ -116,14 +118,14 @@ class MainDriver(object):
         # axcbK.set_label(lab)
         # plt.show(block=False)
         #
-        # figGJ, axGJ, axcbGJ =viz.plotConnectionData(cells, zdata=sim.gjopen, colorbar=1, pickable =True)
+        # figGJ, axGJ, axcbGJ =viz.plotConnectionData(cells, zdata_t=sim.gjopen, colorbar=1, pickable =True)
         # axGJ.set_ylabel('Spatial y [um]')
         # axGJ.set_xlabel('Spatial x [um]')
         # axGJ.set_title('Gap Junction Open Fraction')
         # axcbGJ.set_label('Gap Junction Open Fraction (1.0 = open, 0.0 = closed)')
         # plt.show(block=False)
 
-        # fig1, ax1, axcb1 = viz.plotMemData(cells, clrmap = cm.coolwarm,zdata='random')
+        # fig1, ax1, axcb1 = viz.plotMemData(cells, clrmap = cm.coolwarm,zdata_t='random')
         # ax1.set_ylabel('Spatial y [um]')
         # ax1.set_xlabel('Spatial x [um]')
         # ax1.set_title('Foo Voltage on Discrete Membrane Domains')
@@ -147,7 +149,7 @@ class MainDriver(object):
         # ax8.set_title('Normal and Tangent Vectors to Membrane Domains')
         # plt.show(block=False)
         #
-        # fig9, ax9, axcb9 = viz.plotCellData(zdata='random',pointOverlay=False,edgeOverlay=False)
+        # fig9, ax9, axcb9 = viz.plotCellData(zdata_t='random',pointOverlay=False,edgeOverlay=False)
         # ax9.set_ylabel('Spatial y [um]')
         # ax9.set_xlabel('Spatial x [um]')
         # ax9.set_title('Foo Concentration in Cells Interpolated to Surface Plot')

betse/science/compute.py
@@ -8,7 +8,6 @@
 # FIXME Ion channels
 # FIXME Calcium dynamics
 # FIXME ECM diffusion and discrete membrane domains?
-# FIXME there seems to be some kind of aliasing from one side to the other?
 
 import numpy as np
 import os, os.path
@@ -277,25 +276,6 @@ class Simulator(object):
         self.envV = np.zeros(len(cells.cell_i))
         self.envV[:] = p.vol_env
 
-        # Create vectors holding a range of ion-matched data
-        # self.cc_cells = [cNa_cells,cK_cells,cCl_cells,cCa_cells,cH_cells,cP_cells,cM_cells]  # cell concentrations
-        # self.cc_env = [cNa_env,cK_env,cCl_env,cCa_env,cH_env,cP_env,cM_env]   # environmental concentrations
-        # self.zs = [p.z_Na, p.z_K, p.z_Cl, p.z_Ca, p.z_H, p.z_P, p.z_M]   # matched ion valence state
-        # self.Dm_cells = [DmNa, DmK, DmCl,DmCa,DmH,DmP,DmM]              # matched membrane diffusion constants
-
-        # self.iNa=0     # indices to each ion for use in above arrays
-        # self.iK = 1
-        # self.iCl=2
-        # self.iCa = 3
-        # self.iH = 4
-        # self.iP = 5
-        # self.iM = 6
-
-        # self.movingIons = [self.iNa,self.iK,self.iCl,self.iCa,self.iH,self.iM]
-
-        # self.ionlabel = {self.iNa:'Sodium',self.iK:'Potassium',self.iCl:'Chloride',self.iCa:'Calcium',self.iH:'Proton',
-        #     self.iP:'Protein',self.iM:'Charge Balance Anion'}
-
         # gap junction specific arrays:
         self.gjopen = np.ones(len(cells.gj_i))   # holds gap junction open fraction for each gj
 
@@ -336,7 +316,7 @@ class Simulator(object):
 
         tt = np.linspace(0,p.init_tsteps*p.dt,p.init_tsteps)
         # report
-        print('Your sim initialization is running for', int((p.init_tsteps*p.dt)/60),'minutes of in-world time.')
+        print('Your sim initialization is running for', round((p.init_tsteps*p.dt)/60,2),'minutes of in-world time.')
 
 
         for t in tt:   # run through the loop

betse/science/visualize.py
@@ -5,7 +5,8 @@
 # FIXME create a plotting method that plots individual cell data
 # FIXME create a plotting method for ecm data
 # FIXME work on the vector plotting -- perhaps interpolating data to grid?
-# FIXME do something about colorbars
+# FIXME saving animations doesn't work
+# FIXME do an animation for smoothed 'vert' data and gap junctions (with fluxes)
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,6 +14,72 @@ from matplotlib.collections import LineCollection, PolyCollection
 import matplotlib.cm as cm
 from betse.science import toolbox as tb
 from betse.science.parameters import params as p
+from matplotlib import animation
+
+class AnimateCellData(object):
+    """
+    Animate color data on a plot of cells.
+
+    """
+
+    def __init__(self,cells,zdata_t,tvect,p,colormap=cm.rainbow, save=False,ani_repeat=False):
+
+        self.zdata_t = zdata_t
+        self.colormap = colormap
+        self.time = tvect
+
+        # define a polygon collection based on individual cell polygons
+        self.points = np.multiply(cells.cell_verts, p.um)
+        self.collection =  PolyCollection(self.points, cmap=self.colormap, edgecolors='none')
+        self.collection.set_array(zdata_t[0])
+
+        # set range of the colormap
+        self.cmean = np.mean(self.zdata_t)
+        self.cmin = np.min(self.zdata_t)
+        self.cmax = np.max(self.zdata_t)
+
+        self.fig = plt.figure()       # define figure
+        self.ax = plt.subplot(111)    # define axes
+        self.collection.set_clim(self.cmin,self.cmax)
+
+        self.cb = self.fig.colorbar(self.collection)   # define colorbar for figure
+
+        self.ax.add_collection(self.collection)
+
+        self.ax.set_xlabel('Spatial x [um]')
+        self.ax.set_ylabel('Spatial y [um')
+
+        # sanorm = cells.cell_area/np.linalg.norm(cells.cell_area)
+        #
+        # for sa,pt in zip(sanorm,cells.cell_centres):
+        #     sa = round(sa,2)
+        #     self.ax.text(pt[0]*p.um,pt[1]*p.um,sa)
+
+        self.ax.autoscale_view()
+
+        self.frames = len(self.zdata_t)
+
+        ani = animation.FuncAnimation(self.fig, self.aniFunc,
+               frames=self.frames, interval=66, repeat=ani_repeat)
+
+        if save == True:
+
+            ani.save('basic_animation.mp4')
+            print('Animation saved to file.')
+
+        plt.show()
+
+
+
+    def aniFunc(self,i):
+
+        zz = self.zdata_t[i]
+
+        self.collection.set_array(zz)
+        tit = 'Simulation time' + ' ' + str(round(self.time[i],1)) + ' ' + 's'
+        self.ax.set_title(tit)
+
+        return self.collection
 
 def plotPolyData(cells, fig=None, ax=None, zdata = None,clrmap = None):
         """
@@ -23,7 +90,7 @@ def plotPolyData(cells, fig=None, ax=None, zdata = None,clrmap = None):
         vor_verts              Nested list of [x,y] points defining each polygon. May be ecm_verts or
                                cell_verts
 
-        zdata                  A data array with each scalar entry corresponding to a polygon entry in
+        zdata_t                  A data array with each scalar entry corresponding to a polygon entry in
                                vor_verts. If not specified the default is z=1. If 'random'
                                is specified the method creates random vales from 0 to 1..
 
@@ -64,18 +131,30 @@ def plotPolyData(cells, fig=None, ax=None, zdata = None,clrmap = None):
         points = np.multiply(cells.cell_verts, p.um)
 
         coll = PolyCollection(points, array=z, cmap=clrmap, edgecolors='none')
-        coll.set_picker(True)
+        # coll.set_picker(True)
         ax.add_collection(coll)
         ax.axis('equal')
 
-
         # Add a colorbar for the PolyCollection
         std = np.std(zdata,axis=0)
+        mean = np.mean(zdata,axis=0)
 
-        if zdata != None and std !=0.0:
-            ax_cb = fig.colorbar(coll, ax=ax)
-        else:
-            ax_cb = None
+        if zdata != None and std > 1e-13:
+            ax_cb = fig.colorbar(coll,ax=ax)
+
+        elif std<1e-13:
+
+            if mean < 0:
+                coll.set_clim(mean,0)
+            if mean > 0:
+                coll.set_clim(0,mean)
+
+            ax_cb = fig.colorbar(coll,ax=ax)
+
+        elif zdata == None:
+            coll.set_clim(0,1)
+            ax_cb = fig.colorbar(coll,ax=ax)
+
 
         ax.autoscale_view(tight=True)
 
@@ -89,7 +168,7 @@ def plotCellData(cells, fig=None, ax=None, zdata=None,clrmap=None,edgeOverlay = 
 
         Parameters
         ----------
-        zdata                  A data array with each scalar entry corresponding to a point in
+        zdata_t                  A data array with each scalar entry corresponding to a point in
                                cell_centres. If not specified the default is z=1. If 'random'
                                is specified the method creates random vales from 0 to 1..
 
@@ -169,7 +248,7 @@ def plotMemData(cells, fig= None, ax = None, zdata=None,clrmap=None):
 
         Parameters
         ----------
-        zdata                  A data array with each scalar entry corresponding to a polygon entry in
+        zdata_t                  A data array with each scalar entry corresponding to a polygon entry in
                                vor_verts. If not specified the default is z=1. If 'random'
                                is specified the method creates random vales from 0 to 1..
 
@@ -231,7 +310,7 @@ def plotConnectionData(cells, fig = None, ax=None, zdata=None,clrmap=None,colorb
         Parameters
         ----------
 
-        zdata                  A data array with each scalar entry corresponding to a polygon entry in
+        zdata_t                  A data array with each scalar entry corresponding to a polygon entry in
                                vor_verts. If not specified the default is z=1. If 'random'
                                is specified the method creates random vales from 0 to 1..
 