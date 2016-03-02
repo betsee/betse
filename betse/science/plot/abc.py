Fixed animate Vmem scaling issue.
And put in a vgCa init function.

betse/science/plot/abc.py
@@ -109,6 +109,7 @@ class PlotCells(object, metaclass=ABCMeta):
         axes_x_label: str = 'Spatial Distance [um]',
         axes_y_label: str = 'Spatial Distance [um]',
         colormap: 'Colormap' = None,
+        # scaling_series: np.nadarray = None,
     ) -> None:
         '''
         Initialize this plot.
@@ -201,6 +202,7 @@ class PlotCells(object, metaclass=ABCMeta):
         self._color_min = color_min
         self._color_max = color_max
         self._colormap = colormap
+        # self._scaling_series = scaling_series
 
         # Classify attributes to be subsequently defined.
         self._color_mappings = color_min
@@ -332,8 +334,8 @@ class PlotCells(object, metaclass=ABCMeta):
             time_series_flat = np.ravel(color_series)
 
             # Overwrite the current minimum and maximum color values.
-            self._color_min = np.min(time_series_flat)
-            self._color_max = np.max(time_series_flat)
+            self._color_min = np.ma.min(time_series_flat)
+            self._color_max = np.ma.max(time_series_flat)
 
         # If a single mappable rather than a list of mappables was passed,
         # convert the former to the latter.
@@ -609,7 +611,7 @@ class PlotCells(object, metaclass=ABCMeta):
             grid_x, grid_y, x, y,
             density=self._p.stream_density,
             linewidth=(3.0*magnitude/magnitude_max) + 0.5,
-            color='k',
+            color=self._p.vcolor,
             cmap=self._colormap,
             arrowsize=1.5,
 

betse/science/plot/anim/anim.py
@@ -426,6 +426,7 @@ class AnimCellsTimeSeries(AnimCells):
         self,
         time_series: np.ndarray,
         is_ecm_ignored: bool = True,
+        scaling_series: np.ndarray = None,
         *args, **kwargs
     ) -> None:
         '''
@@ -479,6 +480,7 @@ class AnimCellsTimeSeries(AnimCells):
             self.collection, self._axes = cell_mesh(
                 data_points, self._axes, self._cells, self._p, self._colormap)
 
+
         #FIXME: Is "frame_count=len(self._sim.time)," effectively *ALWAYS* the
         #case, even for animations currently setting the frame count otherwise?
         #We suspect this might be the case, but let's test this up. Winged sun!
@@ -502,7 +504,7 @@ class AnimCellsTimeSeries(AnimCells):
         self._animate(
             frame_count=len(self._sim.time),
             color_mapping=self.collection,
-            color_series=self._time_series,
+            color_series=scaling_series if scaling_series else self._time_series,
         )
 
 

betse/science/sim_toolbox.py
@@ -3,6 +3,7 @@
 # See "LICENSE" for further details.
 
 import numpy as np
+import numpy.ma as ma
 from betse.exceptions import BetseExceptionSimulation
 from betse.science import toolbox as tb
 from scipy import interpolate as interp
@@ -368,7 +369,9 @@ def vertData(data, cells, p):
     dat_grid = gaussian_filter(dat_grid,1)
 
     # get rid of values that bleed into the environment:
-    dat_grid = np.multiply(dat_grid,cells.maskM)
+    # dat_grid = np.multiply(dat_grid,cells.maskM)
+
+    dat_grid = ma.masked_array(dat_grid, np.logical_not(cells.maskM))
 
     return dat_grid
 

betse/science/tissue/handler.py
@@ -12,7 +12,8 @@ from betse.util.type import types
 from random import shuffle
 from scipy import spatial as sps
 from scipy import interpolate as interp
-from betse.science.tissue.channels import vgSodium, vgPotassium, cagPotassium, vgCalcium, vgSodium_init, vgPotassium_init
+from betse.science.tissue.channels import vgSodium, vgPotassium, cagPotassium, vgCalcium, vgSodium_init, \
+    vgPotassium_init, vgCalcium_init
 
 
 class TissueHandler(object):
@@ -443,11 +444,15 @@ class TissueHandler(object):
             self.target_mask_vgCa = np.zeros(self.data_length)
             self.target_mask_vgCa[self.targets_vgCa] = 1
 
-            self.m_Ca = np.zeros(self.data_length)
-            self.h_Ca = np.zeros(self.data_length)
+            # self.m_Ca = np.zeros(self.data_length)
+            # self.h_Ca = np.zeros(self.data_length)
+            #
+            # self.m_Ca[:] = 1.0000/(1+ np.exp(-((sim.vm*1e3 - 10) + 30.000)/6))
+            # self.h_Ca[:] = 1.0000/(1+ np.exp(((sim.vm*1e3 - 10) + 80.000)/6.4))
 
-            self.m_Ca[:] = 1.0000/(1+ np.exp(-((sim.vm*1e3 - 10) + 30.000)/6))
-            self.h_Ca[:] = 1.0000/(1+ np.exp(((sim.vm*1e3 - 10) + 80.000)/6.4))
+            if p.run_sim is True:
+
+                vgCalcium_init(self,sim,p)
 
         if p.vg_options['K_cag'] != 0:
             self.maxDmKcag = p.vg_options['K_cag'][0]