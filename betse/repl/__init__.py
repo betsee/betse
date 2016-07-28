86b79bfb3dc290312888bd62298325ba5fae085d
Douglas Moore <doug@dglmoore.com>
2016-07-28 14:54:37 -0400

    Move the betse.repl submodule to betse.repl.repls

---
+++ betse/repl/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# --------------------( LICENSE                            )--------------------
+# Copyright 2014-2016 by Alexis Pietak & Cecil Curry
+# See "LICENSE" for further details.
+from betse.repl.repls import start_repl
