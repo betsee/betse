0d1cbdb641f7076f2dd39d40653b8032e1443609
Douglas Moore <doug@dglmoore.com>
2016-07-28 18:53:07 -0400

    Create run_script for use within the REPL
    This function takes a path or sequence of paths to script files
    and sequentially executes them in the local namespace. Depending
    on arguments passed, any namespace-related side-effects produced
    by each script may or may not be discarded.

--- betse/repl/environment.py
+++ betse/repl/environment.py
@@ -196,4 +196,61 @@ def load_sim(source):
     else:
         return load_sim(Parameters(source))
 
+def run_script(scripts, dirty=False, globals=globals(), locals=locals()):
+    '''
+    Run a script (or sequence of scripts) within the local environment.
+    
+    If `scripts` is a sequence of scripts, each is executed in turn. If any
+    script raises an `Exception` then the entire pipeline halts.
+
+    .. caution::
+        Note that there is no way to ensure that a script will be perfectly
+        "clean" as there can be side-effects beyond those make within the local
+        python environment. For example, the script could write to one of the
+        various simulation files. The author sees no good way to ensure that
+        this does not happen.
+
+    Parameters
+    ----------
+    scripts : str or sequence
+        The absolute or relative path of the script, or a sequence of such
+        paths.
+    
+    dirty : bool
+        `True` if local changes made in the script should propagate back to
+        the caller, or `False` if they should be discarded.
+
+    globals : dict
+        A dictionary of variable, function and module bindings to use as the
+        global namespace for the script.
+
+    locals : dict
+        A dictionary of variable, function and module bindings to use as the
+        local namespace for the script. Note that unless `dirty` is `True`,
+        any changes made to the local namespace will be discarded.
+    '''
+    from betse.util.type import types
+    from betse.exceptions import BetseExceptionFunction
+    from betse.util.io.log.logs import log_info, log_exception
+
+    def run_single_script(filename):
+        with open(filename) as f:
+            if dirty:
+                exec(f.read(), globals, locals)
+            else:
+                exec(f.read())
+
+    if types.is_sequence_nonstr(scripts):
+        for script in scripts:
+            log_info("Executing script: \"{}\"".format(script))
+            run_single_script(script)
+            print()
+
+    elif types.is_str(scripts):
+        run_single_script(scripts)
+
+    else:
+        msg = "expected a string or sequence of strings as an argument"
+        raise BetseExceptionFunction(msg)
+
 repl_env = locals()
