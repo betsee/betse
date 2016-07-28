cade5afc122f827fea4b7c861cab542774dd08ab
Douglas Moore <doug@dglmoore.com>
2016-07-28 14:38:00 -0400

    Allow the preferred REPL type as an argument
    One possibility is that the user might provide the preferred REPL
    type as an argument, either from a configuration file or at the
    command line. We added an argument to the `start_repl` function to
    facilitate such a use case.

--- betse/repl.py
+++ betse/repl.py
@@ -5,6 +5,7 @@
 import code
 import readline
 import betse.util.io.log.logs as logs
+from betse.exceptions import BetseExceptionFunction
 
 # Attempt to import the ptpython module
 try:
@@ -13,14 +14,38 @@ try:
 except ImportError:
     __has_ptpython = False
 
-def start_repl():
+def start_repl(repl_type : str = None):
     '''
-    Drop into a REPL
+    Drop into a REPL.
+
+    This function starts up a REPL. If the `repl_type` argument is `None`, then
+    it tries to start `ptpython` and falls back to `code` if this fails. If
+    the `repl_type` argument is `not None`, then it attempts to run the
+    `repl_type` as specified by the string.
+
+    An exception is raised if `repl_type` is not `None`, `'ptpython'` or`'code'`.
+
+    Parameters
+    ----------
+    repl_type : str
+        The type of REPL to prefer, either `None`, `ptpython` or `code`.
     '''
-    if __has_ptpython:
-        start_ptpython_repl()
-    else:
+    if repl_type is None:
+        if __has_ptpython:
+            start_ptpython_repl()
+        else:
+            start_code_repl()
+    elif repl_type == 'ptpython':
+        if __has_ptpython:
+            start_ptpython_repl()
+        else:
+            logs.log_info("The ptpython module does not appear to be installed.")
+            logs.log_info("Falling back to a code-based REPL.")
+            start_code_repl()
+    elif repl_type == 'code':
         start_code_repl()
+    else:
+        raise BetseExceptionFunction("unexpected REPL type: \"{}\"".format(repl_type))
 
 def start_code_repl():
     '''
