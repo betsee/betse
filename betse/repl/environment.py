77229d1289cfbca8bdd8ff3393f0a10a5c6fb69e
Douglas Moore <doug@dglmoore.com>
2016-07-28 15:31:12 -0400

    Create the betse.repl.environment module
    This module provides the namespace for REPLs

---
+++ betse/repl/environment.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# --------------------( LICENSE                            )--------------------
+# Copyright 2014-2016 by Alexis Pietak & Cecil Curry
+# See "LICENSE" for further details.
+'''
+This module provides the environmental context for BETSE REPLs.
+
+Each function and variable in this module is loaded into the `repl_env`
+dictionary via a call to `locals`. This is the only symbol that should
+be imported from this module.
+'''
+
+__name__ = '__betse_repl__'
+
+def quit():
+    '''
+    Gracefully exit the REPL, returning control the the caller.
+    '''
+    raise SystemExit
+
+repl_env = locals()
--- betse/repl/repls.py
+++ betse/repl/repls.py
@@ -6,6 +6,7 @@ import code
 import readline
 import betse.util.io.log.logs as logs
 from betse.exceptions import BetseExceptionFunction
+from betse.repl.environment import repl_env
 
 # Attempt to import the ptpython module
 try:
@@ -51,23 +52,8 @@ def start_code_repl():
     '''
     Start a REPL built around the python `code` module
     '''
-    # We need a function to quit the REPL
-    def quit():
-        raise SystemExit
-
-    # Create an environment to use as our REPL's namespace, and override the
-    # '__name__' variable. This will let use distinguish between interactive
-    # and non-interactive environments.
-    env = {'__name__' : '__betse_repl__'}
-
-    # Add out local variable and function definitions to the REPL's environment
-    env.update(locals())
-
-    # And kick off the REPL. In the event that a `SystemExit` is raised, we
-    # catch it and return gracefully. This is because the `quit` function
-    # defined above raises such an exception for this purpose exactly.
     try:
-        code.interact(banner="", local=env)
+        code.interact(banner="", local=repl_env)
     except SystemExit:
         pass
 
@@ -75,14 +61,7 @@ def start_ptpython_repl():
     '''
     Start a REPL built around the `ptpython` module
     '''
-    # Create an environment to use as our REPL's namespace, and override the
-    # '__name__' variable. This will let use distinguish between interactive
-    # and non-interactive environments.
-    env = {'__name__' : '__betse_repl__'}
-
-    # And kick off the REPL. In the event that a `SystemExit` is raised, we
-    # catch it and return gracefully.
     try:
-        embed(globals=globals(), locals=locals())
+        embed(globals=None, locals=repl_env)
     except SystemExit:
         pass
