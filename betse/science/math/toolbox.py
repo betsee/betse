f9af46623ce34c2b04e9b091da48f3e2e8d991bf
leycec <leycec@gmail.com>
2017-06-03 04:05:03 -0400

    Sutherland-Hodgman polygon clipping designed.
    The new "betse.science.math.geometry" subpackage provides a first-draft
    design of a Numpy-based Sutherland-Hodgman polygon clipping
    implementation. Naturally, nothing works yet. (A googleplex's complexion!)

--- betse/science/math/toolbox.py
+++ betse/science/math/toolbox.py
@@ -81,54 +81,6 @@ def area(p):
     """
     return 0.5 * abs(sum(x0*y1 - x1*y0 for ((x0, y0), (x1, y1)) in zip(p, p[1:] + [p[0]])))
 
-def clip(subjectPolygon, clipPolygon):
-    '''
-    Clips the subject polygon to the boundary defined by the clip polygon.
-
-    Notes
-    -------
-    This function implements the Sutherland-Hodgman polygon clipping algorithm,
-    and largely comes courtesy Rosetta Code:
-    http://rosettacode.org/wiki/Sutherland-Hodgman_polygon_clipping#Python
-    '''
-    def inside(p):
-        return(cp2[0]-cp1[0])*(p[1]-cp1[1]) > (cp2[1]-cp1[1])*(p[0]-cp1[0])
-
-    def computeIntersection():
-        dc = [ cp1[0] - cp2[0], cp1[1] - cp2[1] ]
-        dp = [ s[0] - e[0], s[1] - e[1] ]
-        n1 = cp1[0] * cp2[1] - cp1[1] * cp2[0]
-        n2 = s[0] * e[1] - s[1] * e[0]
-        n3 = 1.0 / (dc[0] * dp[1] - dc[1] * dp[0])
-        return [(n1*dp[0] - n2*dc[0]) * n3, (n1*dp[1] - n2*dc[1]) * n3]
-
-    assert isinstance(subjectPolygon, list)
-    assert isinstance(clipPolygon, list)
-    assert len(subjectPolygon)
-    assert len(clipPolygon)
-
-    outputList = subjectPolygon
-    cp1 = clipPolygon[-1]
-
-    for clipVertex in clipPolygon:
-        cp2 = clipVertex
-        inputList = outputList
-        outputList = []
-        s = inputList[-1]
-
-        for subjectVertex in inputList:
-            e = subjectVertex
-            if inside(e):
-                if not inside(s):
-                    outputList.append(computeIntersection())
-                outputList.append(e)
-            elif inside(s):
-                outputList.append(computeIntersection())
-            s = e
-        cp1 = cp2
-
-    return(outputList)
-
 def alpha_shape(points, alpha):
     """
     Calculate the alpha_shape of a cluster of points in 2D.
