2e7032a7335287427099b09a490c74c5bba75279
leycec <leycec@gmail.com>
2018-05-26 01:39:04 -0400

    BETSE 0.8.1 (Kinder Kaufmann) started.
    The extraneous Ubuntu-specific installation script bundled with this
    repository has now been removed. Our conda-forge-hosted package has
    entirely subsumed all prior installation instructions with a single
    monolthic platform-portable installation solution. (Undermined mines!)

--- betse/metadata.py
+++ betse/metadata.py
@@ -160,13 +160,13 @@ if sys.version_info[:3] < PYTHON_VERSION_MIN_PARTS:
 # For further details, see http://semver.org.
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-VERSION = '0.8.0'
+VERSION = '0.8.1'
 '''
 Human-readable application version as a ``.``-delimited string.
 '''
 
 
-CODENAME = 'Kind Kaufmann'
+CODENAME = 'Kinder Kaufmann'
 '''
 Human-readable code name associated with the current version of this
 application.
