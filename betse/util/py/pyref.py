b83e7be78716f5cdb4d38d7451eeef543aa451e4
leycec <leycec@gmail.com>
2018-07-17 01:33:27 -0400

    Reference API documentation improved.
    The docstring for the betse.util.py.pyref.proxy_weak() function has been
    significantly revised in accordance with the actual behaviour of the
    underlying weakref.proxy() function. (Cavalcade of arcade fires erupt!)

--- betse/util/py/pyref.py
+++ betse/util/py/pyref.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# --------------------( LICENSE                            )--------------------
+# --------------------( LICENSE                           )--------------------
 # Copyright 2014-2018 by Alexis Pietak & Cecil Curry.
 # See "LICENSE" for further details.
 
@@ -7,23 +7,27 @@
 Low-level **reference** (i.e., pointer to the address of an object) facilities.
 '''
 
-# ....................{ IMPORTS                            }....................
+# ....................{ IMPORTS                           }....................
 import weakref
 from betse.util.type.types import WeakRefProxyTypes
 
-# ....................{ GETTERS                            }....................
+# ....................{ GETTERS                           }....................
 def proxy_weak(obj: object) -> WeakRefProxyTypes:
     '''
     Weak reference to the passed object as a proxy object, such that the latter
     transparently masquerades as the former.
 
-    If the passed object is:
+    The reference returned by this function raises a :data:`ReferenceError`
+    exception when Python collects and hence deletes the passed object. If this
+    object is:
 
     * Already a proxy object, this object is returned as is.
     * Callable, a :class:`CallableProxyType` object proxying this object is
       returned.
     * Uncallable, a :class:`ProxyType` object proxying this object is returned.
 
+    Caveats
+    ----------
     This function should *always* be called in lieu of the lower-level
     :func:`weakref.proxy` function, which unsafely raises the following
     exception when passed an existing proxy object:
@@ -39,16 +43,17 @@ def proxy_weak(obj: object) -> WeakRefProxyTypes:
     ----------
     WeakRefProxyTypes
         Weak reference to the passed object as a proxy object. When the passed
-        object is garbage collected, this proxy object yields ``None`` rather
-        than the passed object. This proxy object is unhashable regardless of
-        the hashability of the passed object, minimizing issues arising from the
-        fundamentally mutable nature of weak references and preventing their use
-        as dictionary keys.
+        object is garbage-collected, this proxy object raises a
+        :data:`ReferenceError` exception rather than yielding the passed
+        object. This proxy object is unhashable regardless of the hashability
+        of the passed object, avoiding issues arising from the mutability of
+        weak references and preventing their use as dictionary keys.
     '''
 
-    # If this object is already a weak reference proxy, return the object as is.
-    if isinstance(obj, WeakRefProxyTypes):
-        return obj
-    # Else, return a weak reference proxy to the object.
-    else:
-        return weakref.proxy(obj)
+    # Return...
+    return (
+        # If this object is already a weak reference, this object as is.
+        obj if isinstance(obj, WeakRefProxyTypes) else
+        # Else, a weak reference proxying this object.
+        weakref.proxy(obj)
+    )
