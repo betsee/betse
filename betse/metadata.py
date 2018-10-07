35247f6974b7aa7a95398428b6d51428e15f87bd
leycec <leycec@gmail.com>
2018-10-07 01:28:16 -0400

    BETSE 0.9.1 (Luckier Levin) released.

--- betse/metadata.py
+++ betse/metadata.py
@@ -174,13 +174,13 @@ if sys.version_info[:3] < PYTHON_VERSION_MIN_PARTS:
 # For further details, see http://semver.org.
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-VERSION = '0.9.0'
+VERSION = '0.9.1'
 '''
 Human-readable application version as a ``.``-delimited string.
 '''
 
 
-CODENAME = 'Lucky Levin'
+CODENAME = 'Luckier Levin'
 '''
 Human-readable code name associated with the current version of this
 application.
